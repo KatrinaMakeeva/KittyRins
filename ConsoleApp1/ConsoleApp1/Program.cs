61c513de603d56e758694ad69e3ba11e51aa554f
KittyRin <katerina.makeeva2018@yandex.ru>
2018-12-03 19:41:56 +0300

    Console add

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Вас приветствует программа Екатерины Макеевой");
+            Console.WriteLine("Данная программа определяет уровень вашей заработной платы за месяц");
             Console.ReadKey();
+            int x,y;
+            Console.WriteLine("Введите вашу месячную зарплату");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = 25000;
+            if (x > y)
+            { 
+                Console.WriteLine("Высокая зп");
+            }
+            else
+            {
+                Console.WriteLine("Низкая зп");
+            }
+            Console.ReadKey();
+
+
+
         }
     }
 }
